e9a62cc8531640b568437e3238bdf7f9a956d48b
Rahul.Kumar2319aa <rahul.kumar2319aa@cognizant.com>
2025-12-08 00:05:25 +0530

    image updated

---
+++ src/component/CarComponent.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import car_image from "../assets/car_image.jpg"; 
+
+export function CarComponent(){
+
+    return (
+
+        <>
+        
+        <h2>Car demonstrating app</h2>
+        <img src={car_image}/>
+        </>
+    )
+}
